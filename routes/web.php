c6f7c0432d3bb7f7fef73444b52bcebad8cce74e
Rachman Aria <rachmanaria10@gmail.com>
2026-07-25 23:29:11 +0700

    apalah terserah

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,10 @@ Route::get('/solutions', function () {
     return view('solutions.index');
 })->name('solutions');
 
+Route::get('/about', function () {
+    return view('about.index');
+})->name('about');
+
 Route::get('/news', function () {
     return view('news.index');
 })->name('news');
