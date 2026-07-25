7d29ecf7cc49c450ac67d13c0ee6de436841096d
Marcelino Dhimas Adrian <mdhimas.andrian13@gmail.com>
2026-07-25 20:34:37 +0700

    menambahkan rute baru saya

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,62 @@ Route::get('/news', function () {
     return view('news.index');
 })->name('news');
 
+Route::get('/news/page2', function () {
+    return view('news.page2');
+})->name('news.page2');
+
+Route::get('/news/page3', function () {
+    return view('news.page3');
+})->name('news.page3');
+
+Route::get('/news/detail', function () {
+    return view('news.detail');
+})->name('news.detail');
+
+Route::get('/news/detail32', function () {
+    return view('news.detail32');
+})->name('news.detail32');
+
+Route::get('/news/detail31', function () {
+    return view('news.detail31');
+})->name('news.detail31');
+
+Route::get('/news/detail29', function () {
+    return view('news.detail29');
+})->name('news.detail29');
+
+Route::get('/news/detail28', function () {
+    return view('news.detail28');
+})->name('news.detail28');
+
+Route::get('/news/detail23', function () {
+    return view('news.detail23');
+})->name('news.detail23');
+
+Route::get('/news/detail22', function () {
+    return view('news.detail22');
+})->name('news.detail22');
+
+Route::get('/news/detail21', function () {
+    return view('news.detail21');
+})->name('news.detail21');
+
+Route::get('/news/detail20', function () {
+    return view('news.detail20');
+})->name('news.detail20');
+
+Route::get('/news/detail19', function () {
+    return view('news.detail19');
+})->name('news.detail19');
+
+Route::get('/news/detail18', function () {
+    return view('news.detail18');
+})->name('news.detail18');
+
+Route::get('/news/detail16', function () {
+    return view('news.detail16');
+})->name('news.detail16');
+
 Route::get('/contact', function () {
     return view('contact.index');
 })->name('contact');
