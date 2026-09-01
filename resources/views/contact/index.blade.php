00eeff53632bc947afe9542ae75e0adac0ce266c
Rachman Aria <rachmanaria10@gmail.com>
2026-09-01 09:45:32 +0700

    a

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -17,12 +17,24 @@
         $introText = $content['intro_text'] ?? 'We provide a variety of resources to assist our customers and partners in configuring, designing, enhancing and upgrading products. Customers will receive support services and product consultancy support, technical assistance and product sampling.';
         $introNote = $content['intro_note'] ?? 'Please contact our help-line at these numbers';
 
-        $offices = $content['offices'] ?? [
+                $offices = $content['offices'] ?? [
             [
                 'name' => 'Suryasukses Group Surabaya',
                 'phones' => ['031 5030450 / 99603700'],
                 'address_lines' => ['Jl. Kertajaya 109 Surabaya 60286 - Indonesia', 'Jl. Raya Sedati 97 Sidoarjo - Indonesia'],
                 'map' => 'https://goo.gl/maps/RmAtJontTNyMtMuZ6',
+            ],
+            [
+                'name' => 'Suryasukses Group Jakarta',
+                'phones' => ['021 386 1333'],
+                'address_lines' => ['Jakarta', 'Jl. Cideng Timur No. 70E dan F', 'Jakarta Pusat - Indonesia'],
+                'map' => 'https://maps.app.goo.gl/zoNV8ckCL8zGQuDf6',
+            ],
+            [
+                'name' => 'Suryasukses Group Bekasi',
+                'phones' => ['+62 821-6488-8806'],
+                'address_lines' => ['Kawasan Industri MM2100', 'Jl. Selayar Blk. B3 No.1, Mekarwangi Kec. Cikarang Bar, Bekasi, Jawa Barat 17530'],
+                'map' => 'https://goo.gl/maps/K9uRD4tDpK2Wyphd7',
             ],
         ];
 
